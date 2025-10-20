35ed35b3997774af0fde96015579fbd94e5f4167
RileyHop1 <rileyhop1@gmail.com>
2025-10-20 09:38:23 -0700

    Did basic setup

---
+++ rmain.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+#define MAX_NAME_LEN 50
+#define MAX_FEET_SIZE 100
+
+char myModels[][MAX_NAME_LEN];
+
+float myBatteries[];
+
+float myPositions[][2];
+
+
+int main(void) {
+    
+}
